e70b427e6cbb74765e26579b75a7c28cfd6b42bf
Gitter Dunn <gitterdunn@protonmail.com>
2022-06-30 16:03:24 +0200

    first integration test

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,8 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero_to_prod::run;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
-            .route("/hello/{name}", web::get().to(greet))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run()?.await
 }
