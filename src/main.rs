693833a3ee2905988b26fe18aab04e4b7fa9bcb2
Gitter Dunn <gitterdunn@protonmail.com>
2022-06-30 16:57:55 +0200

    [ci-skip] refactor run to take TcpListener, refactored int test to run with random port every time

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::net::TcpListener;
+
 use zero_to_prod::run;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    let address = format!("127.0.0.1:8080");
+    run(TcpListener::bind(address).unwrap())?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,13 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn test_health_check() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // entirely decoupled from implementation details
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -15,10 +16,13 @@ async fn test_health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero_to_prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero_to_prod::run(listener).expect("Failed to bind address");
     // Launch the server as a background test_health_check
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
