207ff562be88de7c0f32aea8b0f4512c4e3dc9d8
Gitter Dunn <gitterdunn@protonmail.com>
2022-07-01 21:55:33 +0200

    add toml formatter to settings.json

--- migrations/20220701184220_create_subscription_table.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
-CREATE TABLE subscriptions(
-  id uuid NOT NULL,
-  PRIMARY KEY (id),
-)
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -26,26 +26,31 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Launch postgres using Docker
-docker run \
--e POSTGRES_USER=${DB_USER} \
--e POSTGRES_PASSWORD=${DB_PASSWORD} \
--e POSTGRES_DB=${DB_NAME} \
--p "${DB_PORT}":5432 \
--d postgres \
-postgres -N 1000
-# ^ Increased maximum number of connections for testing purposes
+# Skip docker if postgres container is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
+  # ^ Increased maximum number of connections for testing purposes
+fi
 
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-  >&2 echo "Postgres is sitll unavailable - sleeping"
+  >&2 echo "Postgres is still unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 # `sqlx database create` relies on the env var DATABASE_URL being set
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 
 sqlx database create
-sqlx migrate add create_subscription_table
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
