768b48dd80a3ed2043ccaed73cd71a0516646ce6
Gitter Dunn <gitterdunn@protonmail.com>
2022-07-25 09:03:24 +0200

    getting started with telemetry - logs and spans

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero_to_prod::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // 'init' calls 'set_logger', so this is all we need to do
+    // We are falling back to printig all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.yml");
     let connection = PgPool::connect(&configuration.database.get_connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::types::Uuid;
 use sqlx::PgPool;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,6 +11,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     // syntax verifies at compile time
     match sqlx::query!(
         r#"
@@ -23,11 +35,18 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     )
     // .get_ref() returns an immutable reference to the PgConnection in web::Data
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
