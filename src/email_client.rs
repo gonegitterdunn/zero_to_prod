ccd036126dbd84488751f91178fecf4eeec1bb8d
Gitter Dunn <gitterdunn@protonmail.com>
2022-08-03 23:06:36 +0200

    [ci-skip] add wiremock, tokio to dev deps. add unit test to email_client

--- src/email_client.rs
+++ src/email_client.rs
@@ -27,3 +27,34 @@ impl EmailClient {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use fake::faker::internet::en::SafeEmail;
+    use wiremock::MockServer;
+
+    use crate::domain::SubscriberEmail;
+
+    use super::EmailClient;
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(Response_Template::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+    }
+}
