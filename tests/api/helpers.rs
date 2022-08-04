2bc7bc856e73a1db9d067113a44d8046b707c41e
Gitter Dunn <gitterdunn@protonmail.com>
2022-08-04 16:54:14 +0200

    [ci-skip] add new tests for mocking mailgun

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,7 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
+use wiremock::MockServer;
 use zero_to_prod::{
     configuration::{get_configuration, DatabaseSettings},
     startup::{get_connection_pool, Application},
@@ -26,6 +27,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -47,12 +49,17 @@ pub async fn spawn_app() -> TestApp {
     // Thereafter, execution will be skipped
     Lazy::force(&TRACING);
 
+    // mock server instead of mailgun's
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration");
         // use a different db for each test
         c.database.database_name = Uuid::new_v4().to_string();
         // use a random os port
         c.application.port = 0;
+        // use mockserver as email api
+        c.email_client.base_url = email_server.uri();
         c
     };
 
@@ -68,6 +75,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,11 +1,21 @@
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app = spawn_app().await;
-
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/messages"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
 
     // Act
     let response = app.post_subscriptions(body.into()).await;
@@ -72,4 +82,19 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         // assert_eq!(saved.name, "null");
         // assert_eq!(saved.email, "null");
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/messages"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
 }
