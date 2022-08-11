19330686e00b1a7da1da86c218468cc5c87e53c1
Gitter Dunn <gitterdunn@protonmail.com>
2022-08-11 11:43:46 +0200

    [ci-skip] delete from impls for Subscribe error and use thiserror procedural macro instead

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -197,11 +197,11 @@ pub enum SubscribeError {
     SendEmailError(#[from] reqwest::Error),
 }
 
-impl From<String> for SubscribeError {
-    fn from(e: String) -> Self {
-        Self::ValidationError(e)
-    }
-}
+// impl From<String> for SubscribeError {
+//     fn from(e: String) -> Self {
+//         Self::ValidationError(e)
+//     }
+// }
 
 impl std::fmt::Debug for SubscribeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
